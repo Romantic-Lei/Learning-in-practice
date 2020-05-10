04a56245b10a7190ceb5d9ef4e0b2f9332c54cfb
Memories6 <1371910434@qq.com>
2020-05-10 10:30:36 +0800

    CyclicBarrier 做减法

---
+++ 大厂面试(JUC-JVM-GC)/CountDownLatchDemo/src/com/luojia/interview/study/thread/CyclicBarrierDemo.java
@@ -0,0 +1,33 @@
+package com.luojia.interview.study.thread;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+/**
+ * 
+ * @author Romantic-Lei
+ * @create 2020年5月10日
+ */
+public class CyclicBarrierDemo {
+
+	public static void main(String[] args) {
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(7, () -> {
+			System.out.println("执行业务完毕");
+		});
+		
+		for (int i = 1; i <= 7; i++) {
+			final int tempInt = i;
+			new Thread(() -> {
+				System.out.println(Thread.currentThread().getName()+"业务员编号："+tempInt);
+				try {
+					cyclicBarrier.await();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (BrokenBarrierException e) {
+					e.printStackTrace();
+				}
+			}).start();
+		}
+	}
+	
+}
