b27f8602300aff7b068309d310b5cc0da37dd482
Memories6 <1371910434@qq.com>
2020-05-16 14:17:02 +0800

    GC垃圾回收器

---
+++ 大厂面试(JUC-JVM-GC)/GC/src/com/luojia/interview/study/GCDemo.java
@@ -0,0 +1,33 @@
+package com.luojia.interview.study;
+
+import java.util.Random;
+
+/**
+ * 
+ * @author Romantic-Lei
+ * @create 2020年5月16日
+ * 1
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseSerialGC
+ * 
+ * 2
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParNewGC
+ * 
+ * 3
+ * -Xms10m -Xmx10m -XX:+PrintGCDetails -XX:+PrintCommandLineFlags -XX:+UseParallelGC
+ */
+public class GCDemo {
+
+	public static void main(String[] args) {
+		System.out.println("*************GCDemo hello");
+		try {
+			String str = "Hello World";
+			while(true) {
+				str += str + new Random().nextInt(10000);
+				str.intern();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
