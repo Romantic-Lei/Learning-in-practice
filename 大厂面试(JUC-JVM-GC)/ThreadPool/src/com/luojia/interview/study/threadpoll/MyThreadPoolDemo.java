c49e8597ed352164bd3dd58fc1b4e0f3615504d4
Memories6 <1371910434@qq.com>
2020-05-19 15:53:15 +0800

    测试

--- 大厂面试(JUC-JVM-GC)/ThreadPool/src/com/luojia/interview/study/threadpoll/MyThreadPoolDemo.java
+++ 大厂面试(JUC-JVM-GC)/ThreadPool/src/com/luojia/interview/study/threadpoll/MyThreadPoolDemo.java
@@ -15,26 +15,27 @@ import java.util.concurrent.TimeUnit;
 public class MyThreadPoolDemo {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println(Runtime.getRuntime().availableProcessors());
-		
+
 		ExecutorService threadPool = new ThreadPoolExecutor(2, 5, 1, TimeUnit.SECONDS,
 				new LinkedBlockingQueue<Runnable>(3), Executors.defaultThreadFactory(),
 				// new ThreadPoolExecutor.AbortPolicy() // 当一个线程进来发现最大线程数和线程等待数都已占满直接报错
-				// new ThreadPoolExecutor.CallerRunsPolicy()
+				// new ThreadPoolExecutor.CallerRunsPolicy()// “调用者运行”一种调节机制，该策略既不会抛弃任务
 				// new ThreadPoolExecutor.DiscardOldestPolicy()// 抛弃队列中等待最久的，即只处理8（最大线程数+最多等待线程数）个线程
-				new ThreadPoolExecutor.DiscardPolicy());// 抛弃最新来的线程
+				new ThreadPoolExecutor.DiscardPolicy()// 抛弃最新来的线程,即只处理8个线程
+		);
 		try {
 			// 模拟10个顾客来办理业务，受理窗口只有5个
 			for (int i = 1; i <= 10; i++) {
 				final int temp = i;
 				threadPool.execute(() -> {
 					System.out.println(Thread.currentThread().getName() + "号窗口，" + "服务顾客" + temp);
-					// try {
-					// TimeUnit.SECONDS.sleep(4);
-					// } catch (InterruptedException e) {
-					// e.printStackTrace();
-					// }
+					try {
+						TimeUnit.SECONDS.sleep(2);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				});
 			}
 		} catch (Exception e) {
