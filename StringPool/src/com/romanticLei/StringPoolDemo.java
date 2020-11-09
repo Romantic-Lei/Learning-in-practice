d939d97d7e6d5f08d7224a84569ce68c673861c0
Romantic-Lei <1371910434@qq.com>
2020-11-09 22:36:33 +0800

    System代码解析

--- StringPool/src/com/romanticLei/StringPoolDemo.java
+++ StringPool/src/com/romanticLei/StringPoolDemo.java
@@ -3,15 +3,23 @@ package com.romanticLei;
 public class StringPoolDemo {
 
     public static void main(String[] args) {
-        String str1 = new StringBuilder("Hello").append("romanticLei").toString();
+        String str1 = new StringBuilder("Hello").append("RomanticLei").toString();
+        // HelloRomanticLei
         System.out.println(str1);
+        // HelloRomanticLei
         System.out.println(str1.intern());
+        // true
+        System.out.println(str1 == str1.intern());
 
         System.out.println();
 
-        String str2 = new StringBuffer("ja").append("va").toString();
+        String str2 = new StringBuilder("ja").append("va").toString();
+        // java
         System.out.println(str2);
+        // java
         System.out.println(str2.intern());
+        // false
+        System.out.println(str2 == str2.intern());
 
     }
 }
