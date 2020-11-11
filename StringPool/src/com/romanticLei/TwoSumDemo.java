aab908ebdd10a37cfe73cea0abc8894ab472e87f
Romantic-Lei <1371910434@qq.com>
2020-11-11 20:32:43 +0800

    两数求和 Hash破解

--- StringPool/src/com/romanticLei/TwoSumDemo.java
+++ StringPool/src/com/romanticLei/TwoSumDemo.java
@@ -1,6 +1,8 @@
 package com.romanticLei;
 
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标
@@ -30,6 +32,26 @@ public class TwoSumDemo {
                 }
             }
         }
+        return null;
+    }
+
+    /**
+     * Hash破解
+     * @param nums
+     * @param target
+     * @return
+     */
+    public static int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++){
+            int partnerNumber = target - nums[i];
+            if (map.containsKey(partnerNumber)){
+                return new int[]{map.get(partnerNumber), i};
+            }
+            map.put(nums[i], i);
+        }
+
         return null;
     }
 
