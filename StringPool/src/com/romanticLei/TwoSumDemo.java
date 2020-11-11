c6e7950a0e7a56a1892041b159100910c01dee9a
Romantic-Lei <1371910434@qq.com>
2020-11-11 11:21:20 +0800

    暴力破解 两数相加找数组下标

---
+++ StringPool/src/com/romanticLei/TwoSumDemo.java
@@ -0,0 +1,51 @@
+package com.romanticLei;
+
+import java.util.Collections;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * @author Romantic-Lei
+ * @version 1.0
+ * @date 2020/11/11
+ *
+ */
+public class TwoSumDemo {
+
+    /**
+     * 遍历----> 暴力破解。。。
+     *
+     * @param nums      集合数组
+     * @param target    目标值
+     * @return
+     */
+    public static int[] twoSum1(int[] nums, int target){
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target - nums[i] == nums[j]){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,11,7,15};
+        int target = 9;
+
+        int[] myIndex = twoSum1(nums, target);
+
+        if (myIndex != null){
+            for (int element : myIndex) {
+                System.out.println(element);
+            }
+        }else {
+            System.out.println("未找到");
+        }
+    }
+
+}
