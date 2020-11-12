922d785f5932f0df3266982fe03d6b9412fa68ab
Romantic-Lei <1371910434@qq.com>
2020-11-12 17:17:26 +0800

    可重入锁 --> 同步方法

--- StringPool/src/com/romanticLei/ReEnterLockDemo.java
+++ StringPool/src/com/romanticLei/ReEnterLockDemo.java
@@ -8,8 +8,8 @@ package com.romanticLei;
 public class ReEnterLockDemo {
 
     static Object objectLockA = new Object();
-    static Object objectLockB = new Object();
 
+    // 同步代码块
     public static void m1(){
         new Thread(() -> {
             synchronized (objectLockA){
@@ -24,7 +24,24 @@ public class ReEnterLockDemo {
         }, "t1").start();
     }
 
+    // 同步方法
+    public synchronized void m2(){
+        System.out.println("=====外");
+        m3();
+    }
+
+    public synchronized void m3(){
+        System.out.println("=====中");
+        m5();
+    }
+
+    public synchronized void m5(){
+        System.out.println("=====内");
+    }
+
+
     public static void main(String[] args) {
         m1();
+//        new ReEnterLockDemo().m2();
     }
 }
