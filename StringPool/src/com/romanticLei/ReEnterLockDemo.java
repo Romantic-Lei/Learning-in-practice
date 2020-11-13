44cdc62cc51ac8549a75d6ab85e3c6e9a364fbd4
Romantic-Lei <1371910434@qq.com>
2020-11-13 11:32:01 +0800

    同步方法 Lock

--- StringPool/src/com/romanticLei/ReEnterLockDemo.java
+++ StringPool/src/com/romanticLei/ReEnterLockDemo.java
@@ -1,5 +1,8 @@
 package com.romanticLei;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author Romantic-Lei
  * @version 1.0
@@ -39,9 +42,36 @@ public class ReEnterLockDemo {
         System.out.println("=====内");
     }
 
+    static Lock lock = new ReentrantLock();
+    public void sycMethod(){
+        lock.lock();
+//        lock.lock();
+        try {
+            System.out.println(Thread.currentThread().getName() + "----外层调用lock");
+            lock.lock();
+            try {
+                System.out.println("----内层调用lock");
+            } finally {
+                lock.unlock();
+            }
+        } finally {
+            lock.unlock();
+            // 有lock加锁就需要有相应的解锁，不然会造成死锁
+            // 如果解锁次数多于加锁次数 会出现异常java.lang.IllegalMonitorStateException
+            // 此处故意让加锁次数和解锁次数不一致，让后面线程等待
+            lock.unlock();
+        }
+    }
 
     public static void main(String[] args) {
-        m1();
+//        m1();
 //        new ReEnterLockDemo().m2();
+        new Thread(() -> {
+            new ReEnterLockDemo().sycMethod();
+        }, "t1").start();
+
+        new Thread(() -> {
+            new ReEnterLockDemo().sycMethod();
+        }, "t2").start();
     }
 }
