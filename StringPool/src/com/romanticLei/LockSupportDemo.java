d57d39633ef8201ed86b19579ea60fd6c3587073
Romantic-Lei <1371910434@qq.com>
2020-11-14 20:49:32 +0800

    线程休眠 1s

--- StringPool/src/com/romanticLei/LockSupportDemo.java
+++ StringPool/src/com/romanticLei/LockSupportDemo.java
@@ -39,6 +39,11 @@ public class LockSupportDemo {
 
     public static void synchronizedAwaitSignal(){
         new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             lock.lock();
             System.out.println(Thread.currentThread().getName() + "\t" + "-----come in");
             try {
