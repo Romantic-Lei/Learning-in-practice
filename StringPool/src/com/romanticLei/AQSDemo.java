7bfc3e6b26a16d592823d3252a6210395187acf1
Romantic-Lei <1371910434@qq.com>
2020-11-17 23:04:34 +0800

    AQS

---
+++ StringPool/src/com/romanticLei/AQSDemo.java
@@ -0,0 +1,15 @@
+package com.romanticLei;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Romantic-Lei
+ * @version 1.0
+ * @date 2020/11/17
+ */
+public class AQSDemo {
+
+    public static void main(String[] args) {
+        ReentrantLock lock = new ReentrantLock();
+    }
+}
