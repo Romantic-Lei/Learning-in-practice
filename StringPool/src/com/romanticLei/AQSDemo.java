10fe4b1b482c32f28dcb9a1e516a5f7672d5d7b0
Romantic-Lei <1371910434@qq.com>
2020-11-20 21:11:43 +0800

    模拟银行窗口排队

--- StringPool/src/com/romanticLei/AQSDemo.java
+++ StringPool/src/com/romanticLei/AQSDemo.java
@@ -1,5 +1,6 @@
 package com.romanticLei;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -11,5 +12,46 @@ public class AQSDemo {
 
     public static void main(String[] args) {
         ReentrantLock lock = new ReentrantLock();
+
+        // 带入一个银行办理业务的案例来模拟我们的AQS如何进行线程的管理和通知唤醒机制
+
+        // 3个线程模拟3个银行网点，受理窗口办理业务的顾客
+
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("-----A thread come in");
+                // 线程暂停整分钟
+                try {
+                    TimeUnit.MINUTES.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            } finally {
+                lock.unlock();
+            }
+        }, "A").start();
+
+        // 第2个顾客
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("-----B thread come in");
+            } finally {
+                lock.unlock();
+            }
+        }, "B").start();
+
+        // 第3个顾客
+        new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println("-----C thread come in");
+            } finally {
+                lock.unlock();
+            }
+        }, "C").start();
+
     }
 }
