c471ac5181d7cf3a73f51763964b4054a37c74e1
romantic <1371910434@qq.com>
2020-11-29 21:42:11 +0800

    springboot

---
+++ interview5/src/main/java/com/romanticlei/study/interview5/Interview5Application.java
@@ -0,0 +1,13 @@
+package com.romanticlei.study.interview5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interview5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Interview5Application.class, args);
+    }
+
+}
---
+++ interview5/src/test/java/com/romanticlei/study/interview5/Interview5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanticlei.study.interview5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interview5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
