f55f5910f59a22fd320bdadac1a66a2316adec46
Romantic-Lei <1371910434@qq.com>
2020-11-30 17:41:45 +0800

    springboot 启动类注解修正

--- interview/src/main/java/com/romanticlei/study/interview/InterviewApplication.java
+++ interview/src/main/java/com/romanticlei/study/interview/InterviewApplication.java
@@ -1,11 +1,15 @@
 package com.romanticlei.study.interview;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class InterviewApplication {
 
     public static void main(String[] args) {
+        System.out.println("服务启动开始");
         SpringApplication.run(InterviewApplication.class, args);
+        System.out.println("服务启动完成");
     }
 
 }
--- interview/src/test/java/com/romanticlei/study/interview/InterviewApplicationTests.java
+++ interview/src/test/java/com/romanticlei/study/interview/InterviewApplicationTests.java
@@ -1,7 +1,9 @@
 package com.romanticlei.study.interview;
 
 import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class InterviewApplicationTests {
 
     @Test
