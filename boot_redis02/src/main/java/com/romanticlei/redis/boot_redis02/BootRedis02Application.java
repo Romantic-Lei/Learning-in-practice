032fd446cf0fdef3516f5db381370bdc9b8369dd
Romantic-Lei <1371910434@qq.com>
2020-12-09 17:33:44 +0800

    复制一份redis01文件

---
+++ boot_redis02/src/main/java/com/romanticlei/redis/boot_redis02/BootRedis02Application.java
@@ -0,0 +1,13 @@
+package com.romanticlei.redis.boot_redis02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedis02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis02Application.class, args);
+    }
+
+}
---
+++ boot_redis02/src/main/java/com/romanticlei/redis/boot_redis02/controller/GoodController.java
@@ -0,0 +1,33 @@
+package com.romanticlei.redis.boot_redis02.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GoodController {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/buy_goods")
+    public String buy_Goods(){
+        String result = stringRedisTemplate.opsForValue().get("goods:001");
+        int goodNumber = result == null ? 0 : Integer.parseInt(result);
+
+        if (goodNumber > 0){
+            int realNumber = goodNumber - 1;
+            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+            System.out.println("成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort);
+            return "成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort;
+        }
+
+        return "商品已经售完"+ "\t 服务提供端口" + serverPort;
+    }
+
+}
