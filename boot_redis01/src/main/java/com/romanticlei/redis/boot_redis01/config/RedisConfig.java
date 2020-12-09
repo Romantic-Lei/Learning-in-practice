37eda9a0bfcc8c03060886cc84382238db6f0155
Romantic-Lei <1371910434@qq.com>
2020-12-09 11:00:03 +0800

    springboot redis配置类

---
+++ boot_redis01/src/main/java/com/romanticlei/redis/boot_redis01/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.romanticlei.redis.boot_redis01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+@Configuration
+public class RedisConfig {
+
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory){
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return redisTemplate;
+    }
+}
+
