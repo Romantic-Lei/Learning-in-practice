7c92076f6309e106adc888f56b24b60dd39f235f
Romantic-Lei <1371910434@qq.com>
2020-12-09 10:59:37 +0800

    springboot 启动配置

---
+++ boot_redis01/src/main/java/com/romanticlei/redis/boot_redis01/BootRedis01Application.java
@@ -0,0 +1,13 @@
+package com.romanticlei.redis.boot_redis01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedis01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedis01Application.class, args);
+    }
+
+}
---
+++ boot_redis01/src/test/java/com/romanticlei/redis/boot_redis01/BootRedis01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanticlei.redis.boot_redis01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRedis01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
