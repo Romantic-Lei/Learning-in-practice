fb82d61c66a3f0cc6939ce274dced9ea90b0408d
Romantic-Lei <1371910434@qq.com>
2020-12-18 10:16:34 +0800

    在代码层面finally 释放锁

--- boot_redis02/src/main/java/com/romanticlei/redis/boot_redis02/controller/GoodController.java
+++ boot_redis02/src/main/java/com/romanticlei/redis/boot_redis02/controller/GoodController.java
@@ -22,25 +22,29 @@ public class GoodController {
     public String buy_Goods() {
 
         String value = UUID.randomUUID().toString() + Thread.currentThread().getName();
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// 该命令就是redis中的setnx
+        try {
+            Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);// 该命令就是redis中的setnx
 
-        if (!flag) {
-            return "抢占失败，请重试！";
-        }
+            if (!flag) {
+                return "抢占失败，请重试！";
+            }
 
-        String result = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodNumber = result == null ? 0 : Integer.parseInt(result);
+            String result = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodNumber = result == null ? 0 : Integer.parseInt(result);
 
-        if (goodNumber > 0) {
-            int realNumber = goodNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
-            System.out.println("成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort);
+            if (goodNumber > 0) {
+                int realNumber = goodNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+                System.out.println("成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort);
+                stringRedisTemplate.delete(REDIS_LOCK);
+
+                return "成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort;
+            }
+
+            return "商品已经售完" + "\t 服务提供端口" + serverPort;
+        } finally {
             stringRedisTemplate.delete(REDIS_LOCK);
-
-            return "成功买到商品，库存还剩下： " + realNumber + "件" + "\t 服务提供端口" + serverPort;
         }
-
-        return "商品已经售完" + "\t 服务提供端口" + serverPort;
     }
 
 }
