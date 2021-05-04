d65812166f0752ae8cea5e60735fc95ee056bcc8
Romantic-Lei <1371910434@qq.com>
2021-05-04 18:20:28 +0800

    基数排序

---
+++ Data Structure and algorithm/DataStructure/src/com/romanticlei/sort/RadixSort.java
@@ -0,0 +1,17 @@
+package com.romanticlei.sort;
+
+public class RadixSort {
+
+    public static void main(String[] args) {
+        int[] arr = {53, 3, 542, 748, 14, 214};
+    }
+
+    // 基数排序方法
+    public static void radixSort() {
+        // 定义一个二维数组，表示十个桶，每个桶就是一个一维数组
+        // 说明：二维数组包含十个桶
+        // 为了防止放入数据时，数据溢出，则每个一维数组（桶）的大小定位arr.length
+        // 所以很明确，基数排序就是使用的空间换时间的算法
+
+    }
+}
