fb27f3f659670c6d34aaf8dd78c417703dfef13c
romantic <1371910434@qq.com>
2021-05-09 13:19:48 +0800

    线性查找算法

---
+++ Data Structure and algorithm/DataStructure/src/com/romanticlei/search/SeqSearch.java
@@ -0,0 +1,23 @@
+package com.romanticlei.search;
+
+public class SeqSearch {
+
+    public static void main(String[] args) {
+        int arr[] = {1, 9, 11, -1, 34, 89};
+        int index = seqSearch(arr, 11);
+        if (index == -1) {
+            System.out.println("没有找到对应的数据");
+        } else {
+            System.out.println("找到，下标为： " + index);
+        }
+    }
+
+    public static int seqSearch(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
