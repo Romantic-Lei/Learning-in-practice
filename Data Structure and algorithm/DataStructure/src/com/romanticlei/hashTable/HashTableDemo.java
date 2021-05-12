ec8629ef06297defa20f6fb9522ce84d3b1a87e5
Romantic-Lei <1371910434@qq.com>
2021-05-12 16:54:32 +0800

    哈希链表删除操作

--- Data Structure and algorithm/DataStructure/src/com/romanticlei/hashTable/HashTableDemo.java
+++ Data Structure and algorithm/DataStructure/src/com/romanticlei/hashTable/HashTableDemo.java
@@ -10,10 +10,11 @@ public class HashTableDemo {
         String key = "";
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("add:  添加雇员");
-            System.out.println("find: 查找雇员");
-            System.out.println("list: 显示雇员");
-            System.out.println("exit: 退出系统");
+            System.out.println("add:    添加雇员");
+            System.out.println("find:   查找雇员");
+            System.out.println("list:   显示雇员");
+            System.out.println("delete: 删除雇员");
+            System.out.println("exit:   退出系统");
 
             key = scanner.next();
             switch (key) {
@@ -33,6 +34,11 @@ public class HashTableDemo {
                 case "list":
                     hashTable.list();
                     break;
+                case "delete":
+                    System.out.println("请输入要删除的id");
+                    id = scanner.nextInt();
+                    hashTable.deleteById(id);
+                    break;
                 case "exit":
                     System.exit(0);
                     break;
@@ -44,7 +50,7 @@ public class HashTableDemo {
 }
 
 // 创建一个 HashTable 管理多个链表
-class HashTable{
+class HashTable {
     private EmpLinkedList[] empLinkedListArray;
     private int size; // 集合的个数
 
@@ -66,7 +72,7 @@ class HashTable{
         empLinkedListArray[empLinkedListNo].add(emp);
     }
 
-    public void list(){
+    public void list() {
         for (int i = 0; i < size; i++) {
             empLinkedListArray[i].list(i);
         }
@@ -82,13 +88,20 @@ class HashTable{
         }
     }
 
+    public void deleteById(int id) {
+        int empLinkedListNo = hashFun(id);
+
+        String s = empLinkedListArray[empLinkedListNo].deleteById(id);
+        System.out.println(s);
+    }
+
     // 编写散列函数，使用一个简单取模算法
     public int hashFun(int id) {
         return id % size;
     }
 }
 
-class Emp{
+class Emp {
     public int id;
     public String name;
     public Emp next; // 下一个员工信息，默认为null
@@ -100,7 +113,7 @@ class Emp{
 }
 
 // 创建 EmpLinkedList， 表示链表
-class EmpLinkedList{
+class EmpLinkedList {
     // 头指针，执行第一个Emp，因此我们这个链表的 head 是直接指向第一个Emp
     private Emp head;
 
@@ -121,7 +134,7 @@ class EmpLinkedList{
     }
 
     // 遍历链表
-    public void list(int no){
+    public void list(int no) {
         if (head == null) {
             System.out.println("第" + (no + 1) + "条链表为空");
             return;
@@ -155,5 +168,42 @@ class EmpLinkedList{
         }
 
         return null;
+    }
+
+    // 根据 id 删除雇员信息
+    public String deleteById(int id) {
+        if (head == null) {
+            return "链表为空！";
+        }
+
+        Emp curEmp = head;
+        // 当前头结点id 就是需要删除的id
+        if (curEmp.id == id) {
+            // 当前链就不止只有头结点一条数据
+            if (curEmp.next != null) {
+                head = curEmp.next;
+            } else {
+                head = null;
+            }
+            return "删除成功！";
+        }
+
+        while (curEmp.next != null){
+            if (curEmp.next.id == id) {
+                // 判断删除节点不是当前链表的最后一条数据
+                if (curEmp.next.next != null){
+                    curEmp.next = curEmp.next.next;
+                } else {
+                    // 需要删除的数据就是链表的最后一条数据集
+                    curEmp.next = null;
+                }
+
+                return "删除成功！";
+            }
+
+            curEmp = curEmp.next;
+        }
+
+        return "没有找到该雇员！";
     }
 }
