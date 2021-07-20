0edbc10051c4154e53e3ba2844fbb2c54b5cd70e
Romantic-Lei <1371910434@qq.com>
2021-07-20 14:39:07 +0800

    二叉排序树的新增和遍历

--- Data Structure and algorithm/DataStructure/src/com/romanticlei/binarysorttree/BinarySortTreeDemo.java
+++ Data Structure and algorithm/DataStructure/src/com/romanticlei/binarysorttree/BinarySortTreeDemo.java
@@ -2,7 +2,37 @@ package com.romanticlei.binarysorttree;
 
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
+        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        for (int i = 0; i < arr.length; i++) {
+            binarySortTree.addNode(new Node(arr[i]));
+        }
 
+        System.out.println("中序遍历二叉排序树~~~");
+        // 1, 3, 5, 7, 9, 10, 12
+        binarySortTree.infixOrder();
+    }
+}
+
+class BinarySortTree {
+    private Node root;
+    // 添加节点的方法
+    public void addNode(Node node) {
+        if (root == null) {
+            // 如果root 为空，则直接让root指向node(即第一个结点进来的时候初始化)
+            root = node;
+        } else {
+            root.addNode(node);
+        }
+    }
+
+    // 中序遍历
+    public void infixOrder() {
+        if (root != null) {
+            root.infixOrder();
+        } else {
+            System.out.println("遍历结点无数据！");
+        }
     }
 }
 
