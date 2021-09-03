c39195bc4f5a3144354f24d9ebb7797974009d29
Romantic-Lei <1371910434@qq.com>
2021-09-03 12:50:09 +0800

    算法

---
+++ Data Structure and algorithm/Algorithm/src/com/romanticlei/greedy/GreedyAlgorithm.java
@@ -0,0 +1,17 @@
+package com.romanticlei.greedy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class GreedyAlgorithm {
+
+    public static void main(String[] args) {
+        //创建广播电台,放入到 Map
+        HashMap<String, java.util.HashSet<String>> broadcasts = new HashMap<String, HashSet<String>>();
+        // 将各个电台放入到 broadcasts
+        HashSet<String> hashSet1 = new HashSet<String>();
+        hashSet1.add("北京");
+        hashSet1.add("上海");
+        hashSet1.add("天津");
+    }
+}
