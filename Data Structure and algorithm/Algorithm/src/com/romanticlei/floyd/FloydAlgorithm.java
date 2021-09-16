6cfd98719f68b88125c954572bb2f31d6ad7a396
Romantic-Lei <1371910434@qq.com>
2021-09-16 15:16:33 +0800

    弗洛伊德算法 结果的显示

--- Data Structure and algorithm/Algorithm/src/com/romanticlei/floyd/FloydAlgorithm.java
+++ Data Structure and algorithm/Algorithm/src/com/romanticlei/floyd/FloydAlgorithm.java
@@ -5,7 +5,20 @@ import java.util.Arrays;
 public class FloydAlgorithm {
 
     public static void main(String[] args) {
-
+        // 测试看看图是否创建成功
+        char[] vertex = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+        //创建邻接矩阵
+        int[][] matrix = new int[vertex.length][vertex.length];
+        final int N = 65535;
+        matrix[0] = new int[] { 0, 5, 7, N, N, N, 2 };
+        matrix[1] = new int[] { 5, 0, N, 9, N, N, 3 };
+        matrix[2] = new int[] { 7, N, 0, N, 8, N, N };
+        matrix[3] = new int[] { N, 9, N, 0, N, 4, N };
+        matrix[4] = new int[] { N, N, 8, N, 0, 5, 4 };
+        matrix[5] = new int[] { N, N, N, 4, 5, 0, 6 };
+        matrix[6] = new int[] { 2, 3, N, N, 4, 6, 0 };
+        Graph graph = new Graph(vertex.length, matrix, vertex);
+        graph.show();
     }
 
 }
@@ -30,6 +43,21 @@ class Graph {
         // 对 pre数组进行初始化，数组中存放的是前驱顶点的下标
         for (int i = 0; i < length; i++) {
             Arrays.fill(pre[i], i);
+        }
+    }
+
+    public void show() {
+        for (int k = 0; k < dis.length; k++) {
+            // 先将 pre 数组输出一行
+            for (int i = 0; i < dis.length; i++) {
+                System.out.print(vertexs[pre[k][i]] + " ");
+            }
+            System.out.println();
+
+            for (int i = 0; i < dis.length; i++) {
+                System.out.print(dis[k][i] + " ");
+            }
+            System.out.println();
         }
     }
 
