5b511ae76c907ea9ca229f0d39fa76c312730178
Romantic-Lei <1371910434@qq.com>
2021-09-17 19:03:27 +0800

    马踏棋盘算法

--- Data Structure and algorithm/Algorithm/src/com/romanticlei/horse/HorseChessboard.java
+++ Data Structure and algorithm/Algorithm/src/com/romanticlei/horse/HorseChessboard.java
@@ -7,11 +7,37 @@ public class HorseChessboard {
 
     private static int X;   // 棋盘的列数(从 0 开始)
     private static int Y;   // 棋盘的行数(从 0 开始)
+    // 创建一个数组，标记棋盘的各个位置是否被访问量过
+    private static boolean visited[];
+    // 标记是否棋盘的所有位置都被访问，如果为 true，表示成功
+    private static boolean finished;
 
     public static void main(String[] args) {
 
     }
 
+    /**
+     * 完成骑士周游列国的算法
+     * @param chessboard    棋盘
+     * @param row           马儿当前位置的行 从0开始
+     * @param column         马儿当前位置的列 从0开始
+     * @param step          是第几步，初始位置就是第1步
+     */
+    public static void traversalChessboard(int[][] chessboard, int row, int column, int step) {
+        // 标记当前位置是第几步
+        chessboard[row][column] = step;
+        // 标记该点已访问
+        visited[row * X + column] = true;
+        ArrayList<Point> ps = next(new Point(column, row));
+        
+    }
+
+    /**
+     * 根据当前位置（Point对象），计算马儿还能走那些位置（Point），
+     * 并放入到一个集合中（ArrayList），最多能有8个位置能走
+     * @param curPoint
+     * @return
+     */
     public static ArrayList<Point> next(Point curPoint) {
         // 创建一个 ArrayList
         ArrayList<Point> ps = new ArrayList<Point>();
