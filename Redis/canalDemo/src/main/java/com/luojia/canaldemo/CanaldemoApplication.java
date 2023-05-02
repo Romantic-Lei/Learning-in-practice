c8514a466d1bc75b2298a54ca38775f078403851
romantic <1371910434@qq.com>
2023-05-02 14:31:41 +0800

    canal框架搭建

---
+++ Redis/canalDemo/src/main/java/com/luojia/canaldemo/CanaldemoApplication.java
@@ -0,0 +1,13 @@
+package com.luojia.canaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaldemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanaldemoApplication.class, args);
+    }
+
+}
---
+++ Redis/canalDemo/src/test/java/com/luojia/canaldemo/CanaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luojia.canaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaldemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
