2c921ae69e186c652871e9ed7a1d0ec44c3b4765
romantic <1371910434@qq.com>
2023-05-07 22:38:19 +0800

    mapper文件更新

---
+++ Redis/redis7-study/src/main/java/com/luojia/redis7_study/mapper/CustomerMapper.java
@@ -0,0 +1,7 @@
+package com.luojia.redis7_study.mapper;
+
+import com.luojia.redis7_study.entities.Customer;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CustomerMapper extends Mapper<Customer> {
+}
