49e5ba731a719956a0d58f0980aaf0531e75c8a1
romantic <1371910434@qq.com>
2023-05-09 22:56:45 +0800

    guava过滤器测试类

--- Redis/redis7-study/src/test/java/com/luojia/redis7_study/Redis7StudyApplicationTests.java
+++ Redis/redis7-study/src/test/java/com/luojia/redis7_study/Redis7StudyApplicationTests.java
@@ -1,5 +1,7 @@
 package com.luojia.redis7_study;
 
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,21 @@ class Redis7StudyApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testGuava() {
+        // 1 创建Guava 版本布隆过滤器
+        BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), 100);
+        // 2 判断指定的元素是否存在
+        System.out.println(bloomFilter.mightContain(1));// false
+        System.out.println(bloomFilter.mightContain(2));// false
+        System.out.println();
+        // 3 将元素新增进入布隆过滤器
+        bloomFilter.put(1);
+        bloomFilter.put(2);
+        System.out.println(bloomFilter.mightContain(1));// true
+        System.out.println(bloomFilter.mightContain(2));// true
     }
 
 }
