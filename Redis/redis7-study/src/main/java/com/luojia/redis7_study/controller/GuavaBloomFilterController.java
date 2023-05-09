5c96519531fea68392717516c0a68524bf2bafb5
romantic <1371910434@qq.com>
2023-05-09 22:55:49 +0800

    guava过滤器控制层

--- Redis/redis7-study/src/main/java/com/luojia/redis7_study/controller/GuavaBloomFilterController.java
+++ Redis/redis7-study/src/main/java/com/luojia/redis7_study/controller/GuavaBloomFilterController.java
@@ -1,4 +1,25 @@
 package com.luojia.redis7_study.controller;
 
-public class GuavaBloomFIlterController {
+import com.luojia.redis7_study.service.GuavaBloomFilterService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "google工具Guava处理布隆过滤器")
+@RestController
+@Slf4j
+public class GuavaBloomFilterController {
+
+    @Autowired
+    private GuavaBloomFilterService guavaBloomFilterService;
+
+    @ApiOperation("guava布隆过滤器插入100万样本数据并额外10万测试是否存在")
+    @GetMapping("/guavafilter")
+    public void guavaBloomFIlterService() {
+        guavaBloomFilterService.guavaBloomFilterService();
+    }
+
 }
