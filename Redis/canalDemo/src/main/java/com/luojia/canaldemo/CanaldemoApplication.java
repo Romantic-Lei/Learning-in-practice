e96148e5e1cfb7b0846de6ac43595e9610a6b5ba
romantic <1371910434@qq.com>
2023-05-10 23:17:48 +0800

    canal缺陷修复

--- Redis/canalDemo/src/main/java/com/luojia/canaldemo/CanaldemoApplication.java
+++ Redis/canalDemo/src/main/java/com/luojia/canaldemo/CanaldemoApplication.java
@@ -5,13 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CanaldemoApplication {
 
     // 只是一个canal监听，不需要主启动类
     public static void main(String[] args) {
-        // SpringApplication.run(CanaldemoApplication.class, args);
+        SpringApplication.run(CanaldemoApplication.class, args);
     }
 
 }
