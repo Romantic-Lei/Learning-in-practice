7dba35a4e84da788252916eb9037f537bb3c06e7
romantic <1371910434@qq.com>
2023-05-13 18:26:27 +0800

    商品售卖业务类

---
+++ Redis/12.Redis高阶篇/redis_distributed_lock2/src/main/java/com/luojia/redislock/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.luojia.redislock.controller;
+
+import com.luojia.redislock.service.InventoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "redis分布式锁测试")
+public class InventoryController {
+    @Autowired
+    private InventoryService inventoryService;
+
+    @GetMapping("/sale")
+    @ApiOperation("扣减库存，一次卖一个")
+    public void sale() {
+        inventoryService.sale();
+    }
+}
---
+++ Redis/12.Redis高阶篇/redis_distributed_lock2/src/main/java/com/luojia/redislock/service/InventoryService.java
@@ -0,0 +1,48 @@
+package com.luojia.redislock.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+@Slf4j
+public class InventoryService {
+    
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    @Value("${server.port}")
+    private String port;
+
+    private Lock lock = new ReentrantLock();
+
+    public String sale() {
+        String resMessgae = "";
+        lock.lock();
+        try {
+            // 1 查询库存信息
+            String result = stringRedisTemplate.opsForValue().get("inventory01");
+            // 2 判断库存书否足够
+            Integer inventoryNum = result == null ? 0 : Integer.parseInt(result);
+            // 3 扣减库存，每次减少一个库存
+            if (inventoryNum > 0) {
+                stringRedisTemplate.opsForValue().set("inventory01", String.valueOf(--inventoryNum));
+                resMessgae = "成功卖出一个商品，库存剩余：" + inventoryNum;
+                log.info(resMessgae + "\t" + "，服务端口号：" + port);
+            } else {
+                resMessgae = "商品已售罄。";
+                log.info(resMessgae + "\t" + "，服务端口号：" + port);
+            }
+
+        } finally {
+            lock.unlock();
+        }
+
+        return resMessgae;
+    }
+
+}
