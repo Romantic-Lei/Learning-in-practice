acdc5545cefff7660733939d8ce9901a7ed07628
romantic <1371910434@qq.com>
2023-06-22 18:23:55 +0800

    FutureThreadPoolDemo

--- JUC2022/JUC/src/com/juc/cf/FutureThreadPoolDemo.java
+++ JUC2022/JUC/src/com/juc/cf/FutureThreadPoolDemo.java
@@ -3,7 +3,44 @@ package com.juc.cf;
 import java.util.concurrent.*;
 
 public class FutureThreadPoolDemo {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        // ThreadNum3();
+        // FutureBlock();
+        FutureDone();
+    }
+
+    public static void FutureDone() throws ExecutionException, InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
+            TimeUnit.SECONDS.sleep(5);
+            return "task over";
+        });
+        threadPool.submit(futureTask);
+        System.out.println("-----执行其他任务");
+        while (true) {
+            if (futureTask.isDone()) {
+                System.out.println(futureTask.get());
+                break;
+            } else {
+                TimeUnit.MILLISECONDS.sleep(500);
+                System.out.println("异步线程暂未执行完毕");
+            }
+        }
+    }
+
+    public static void FutureBlock() throws InterruptedException, ExecutionException, TimeoutException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        FutureTask<String> futureTask = new FutureTask<String>(() -> {
+            TimeUnit.SECONDS.sleep(5);
+            return "task over";
+        });
+        threadPool.submit(futureTask);
+        System.out.println("-----执行其他任务");
+        futureTask.get(3, TimeUnit.SECONDS);
+
+    }
+
+    public static void ThreadNum3() throws ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(3);
 
         long startTime = System.currentTimeMillis();
