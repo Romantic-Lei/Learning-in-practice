626583331fa15b3e6eea255fd05ed8e11766da2d
romantic <1371910434@qq.com>
2023-06-23 15:38:55 +0800

    异步线程有返回值

--- JUC2022/JUC/src/com/juc/cf/CompletableFutureDemo.java
+++ JUC2022/JUC/src/com/juc/cf/CompletableFutureDemo.java
@@ -4,14 +4,47 @@ import java.util.concurrent.*;
 
 public class CompletableFutureDemo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        runAsyncNoExecutor();
-        runAsync();
+        // runAsyncNoExecutor();
+        // runAsync();
+        // supplyAsyncNoExecutor();
+        supplyAsync();
+    }
 
+    public static void supplyAsync() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            // pool-1-thread-1
+            System.out.println(Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello supplyAsyncNoExecutor";
+        }, executorService);
+        // hello supplyAsyncNoExecutor
+        System.out.println(completableFuture.get());
+    }
+
+    public static void supplyAsyncNoExecutor() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
+            // ForkJoinPool.commonPool-worker-1
+            System.out.println(Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "hello supplyAsyncNoExecutor";
+        });
+        // hello supplyAsyncNoExecutor
+        System.out.println(completableFuture.get());
     }
 
     public static void runAsync() throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
+            // pool-1-thread-1
             System.out.println(Thread.currentThread().getName());
             try {
                 TimeUnit.SECONDS.sleep(1);
@@ -19,13 +52,14 @@ public class CompletableFutureDemo {
                 e.printStackTrace();
             }
         }, executorService);
-        // completableFuture.get()不调用get方法异步任务不会执行
-        // pool-1-thread-1
+        // null
         System.out.println(completableFuture.get());
+        executorService.shutdown();
     }
 
     public static void runAsyncNoExecutor() throws ExecutionException, InterruptedException {
         CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
+            // ForkJoinPool.commonPool-worker-1
             System.out.println(Thread.currentThread().getName());
             try {
                 TimeUnit.SECONDS.sleep(1);
@@ -33,8 +67,8 @@ public class CompletableFutureDemo {
                 e.printStackTrace();
             }
         });
-        // completableFuture.get()不调用get方法异步任务不会执行
-        // ForkJoinPool.commonPool-worker-1
+
+        // null
         System.out.println(completableFuture.get());
     }
 }
