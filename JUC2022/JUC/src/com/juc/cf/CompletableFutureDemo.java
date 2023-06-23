e8048b7fbdd1be61e2293cfff1489c291a1ed781
romantic <1371910434@qq.com>
2023-06-23 16:14:35 +0800

    异步线程回调

--- JUC2022/JUC/src/com/juc/cf/CompletableFutureDemo.java
+++ JUC2022/JUC/src/com/juc/cf/CompletableFutureDemo.java
@@ -7,7 +7,37 @@ public class CompletableFutureDemo {
         // runAsyncNoExecutor();
         // runAsync();
         // supplyAsyncNoExecutor();
-        supplyAsync();
+        // supplyAsync();
+        supplyAsync1();
+    }
+
+    public static void supplyAsync1() {
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            // pool-1-thread-1
+            System.out.println(Thread.currentThread().getName() + "come in");
+            int result = ThreadLocalRandom.current().nextInt(10);
+            if (result > 5) {
+                int i = 10 / 0;
+            }
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }, executorService).whenComplete((v, e) -> {
+            if (null == e) {
+                System.out.println("------计算完成，未发生异常，结果为：" + v);
+            }
+        }).exceptionally(e -> {
+            e.printStackTrace();
+            System.out.println("系统发生异常" + e.getCause() + "\t" + e.getMessage());
+            return null;
+        });
+
+        System.out.println("main主线程执行自己的其他逻辑");
+        executorService.shutdown();
     }
 
     public static void supplyAsync() throws ExecutionException, InterruptedException {
@@ -24,6 +54,8 @@ public class CompletableFutureDemo {
         }, executorService);
         // hello supplyAsyncNoExecutor
         System.out.println(completableFuture.get());
+        System.out.println("查看是否阻塞了后面的逻辑");
+        executorService.shutdown();
     }
 
     public static void supplyAsyncNoExecutor() throws ExecutionException, InterruptedException {
