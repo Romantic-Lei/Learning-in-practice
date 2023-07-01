eda2563aa0c44a2e575bbefb8924460cbce479e4
romantic <1371910434@qq.com>
2023-07-01 22:40:25 +0800

    锁

--- JUC2022/JUC/src/com/juc/lock/LockSyncDemo.java
+++ JUC2022/JUC/src/com/juc/lock/LockSyncDemo.java
@@ -1,4 +1,23 @@
 package com.juc.lock;
 
 public class LockSyncDemo {
+
+    private final Object object = new Object();
+    // public void m1() {
+    //     synchronized (object) {
+    //         System.out.println("---------hello synchronized code block---------");
+    //     }
+    // }
+
+    // public synchronized void m2() {
+    //     System.out.println("---------hello synchronized code block---------");
+    // }
+
+    public static synchronized void m3() {
+        System.out.println("---------hello static synchronized code block---------");
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
