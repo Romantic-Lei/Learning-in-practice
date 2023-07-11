31f1339d3eaa75079959719b12d058909ab69f66
romantic <1371910434@qq.com>
2023-07-11 23:09:11 +0800

    AtomicMarkableReference

--- JUC2022/JUC/src/com/juc/atomic/AtomicMarkableReferenceDemo.java
+++ JUC2022/JUC/src/com/juc/atomic/AtomicMarkableReferenceDemo.java
@@ -1,4 +1,26 @@
 package com.juc.atomic;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
 public class AtomicMarkableReferenceDemo {
+
+    public static AtomicMarkableReference markableReference = new AtomicMarkableReference(100, false);
+    public static void main(String[] args) {
+        new Thread(() -> {
+            boolean marked = markableReference.isMarked();
+            System.out.println(Thread.currentThread().getName() + "\t" + "默认标识：" + marked);
+            try {TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) {e.printStackTrace();}
+            markableReference.compareAndSet(100, 1000, marked, !marked);
+        }, "t1").start();
+
+        new Thread(() -> {
+            boolean marked = markableReference.isMarked();
+            System.out.println(Thread.currentThread().getName() + "\t" + "默认标识：" + marked);
+            try {TimeUnit.SECONDS.sleep(2);} catch (InterruptedException e) {e.printStackTrace();}
+            boolean b = markableReference.compareAndSet(100, 2000, marked, !marked);
+            System.out.println(Thread.currentThread().getName() + "\t" + "t2线程CASresult： " + b);
+            System.out.println(Thread.currentThread().getName() + "\t" + markableReference.getReference());
+        }, "t2").start();
+    }
 }
