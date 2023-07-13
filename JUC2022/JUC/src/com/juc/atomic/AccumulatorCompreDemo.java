0a69c67351357fc7e032cddddac9c8e5a6e4c010
romantic <1371910434@qq.com>
2023-07-13 23:01:21 +0800

    AccumulatorCompreDemo

--- JUC2022/JUC/src/com/juc/atomic/AccumulatorCompreDemo.java
+++ JUC2022/JUC/src/com/juc/atomic/AccumulatorCompreDemo.java
@@ -1,6 +1,7 @@
 package com.juc.atomic;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAccumulator;
 import java.util.concurrent.atomic.LongAdder;
 
 class ClickNumber {
@@ -19,11 +20,19 @@ class ClickNumber {
         longAdder.increment();
     }
 
+    LongAccumulator longAccumulator = new LongAccumulator((x, y) -> x + y, 0);
+    public void clickLongAccumulator() {
+        longAccumulator.accumulate(1);
+    }
+
 }
 /**
  * 需求：50个线程，每个线程100W次，求总点赞数
  */
 public class AccumulatorCompreDemo {
+
+    public static final int _1W = 10000;
+    public static final int threadNum = 50;
 
     public static void main(String[] args) {
 
