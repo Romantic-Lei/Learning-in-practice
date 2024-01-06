2a0fbc129b1eced256a6ad15ba0918bb3366b078
romantic <1371910434@qq.com>
2024-01-06 20:13:04 +0800

    使用swagger 生成接口文档

--- springBoot3/boot3-robot-starter/src/main/java/com/luojia/boot3robotstarter/robot/controller/RobotController.java
+++ springBoot3/boot3-robot-starter/src/main/java/com/luojia/boot3robotstarter/robot/controller/RobotController.java
@@ -1,16 +1,20 @@
 package com.luojia.boot3robotstarter.robot.controller;
 
 import com.luojia.boot3robotstarter.robot.service.RobotService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "机器控制类")
 @RestController
 public class RobotController {
 
     @Autowired
     RobotService robotService;
 
+    @Operation(summary = "查询", description = "说你好")
     @GetMapping("/robot/hello")
     public String sayHello() {
         return robotService.sayHello();
