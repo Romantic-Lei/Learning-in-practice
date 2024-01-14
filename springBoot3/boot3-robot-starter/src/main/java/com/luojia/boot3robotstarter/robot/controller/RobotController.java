a265de3a571784a4ccf814ca228f78ce2ab08f3c
romantic <1371910434@qq.com>
2024-01-14 15:57:40 +0800

    自定义线程池

--- springBoot3/boot3-robot-starter/src/main/java/com/luojia/boot3robotstarter/robot/controller/RobotController.java
+++ springBoot3/boot3-robot-starter/src/main/java/com/luojia/boot3robotstarter/robot/controller/RobotController.java
@@ -7,6 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 @Tag(name = "机器控制类")
 @RestController
 public class RobotController {
@@ -18,6 +28,26 @@ public class RobotController {
     @GetMapping("/robot/hello")
     public String sayHello() {
         return robotService.sayHello();
+    }
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread("nn");
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 8, 10, TimeUnit.SECONDS
+                , new LinkedBlockingQueue(100),
+                (r -> {
+                    return new Thread(r,"nn");
+                }),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName() + "正在执行");
+                }
+            });
+        }
     }
 
 }
