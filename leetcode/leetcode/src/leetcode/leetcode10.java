dea3a60f9f82ac700b3993b6171a5549d08ee7cb
romantic <1371910434@qq.com>
2024-03-10 15:05:33 +0800

    反转

--- leetcode/leetcode/src/leetcode/leetcode10.java
+++ leetcode/leetcode/src/leetcode/leetcode10.java
@@ -39,6 +39,37 @@ public class leetcode10 {
 
     }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+
+        while (l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+
+            if (carry != 0) {
+                cur.next = new ListNode(carry);
+            }
+
+        }
+
+        return pre.next;
+    }
+
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
