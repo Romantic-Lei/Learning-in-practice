fb62e67a15afc524e000560b019b0a3d01bd0e83
romantic <1371910434@qq.com>
2024-03-10 15:06:16 +0800

    最长子串 的长度

--- leetcode/leetcode/src/leetcode/leetcode10.java
+++ leetcode/leetcode/src/leetcode/leetcode10.java
@@ -39,6 +39,26 @@ public class leetcode10 {
 
     }
 
+    // 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int left = 0;
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                // 将滑动框中首次出现的字母位置移出到框外，取max是为了防止abba这种让left倒退的异常
+                left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+
+            map.put(s.charAt(i), i);
+            // 最长子串与（当前元素位置到滑动框的起始位置相减）比较
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode pre = new ListNode(0);
         ListNode cur = pre;
