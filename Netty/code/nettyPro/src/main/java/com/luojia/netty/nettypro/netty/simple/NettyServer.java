fa1b0def83cc11fcc097847db174ba0309e54d0e
romantic <1371910434@qq.com>
2024-03-18 21:34:24 +0800

    netty 入门服务端

--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyServer.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyServer.java
@@ -20,25 +20,31 @@ public class NettyServer {
 
         // 创建服务器端的启动对象，配置参数
         ServerBootstrap bootstrap = new ServerBootstrap();
-        // 使用链式编程来进行设置
-        bootstrap.group(bossGroup, workerGroup) // 设置两个线程组
-                .channel(NioServerSocketChannel.class) // 使用 NioServerSocketChannel 作为服务器的通道实现
-                .option(ChannelOption.SO_BACKLOG, 128) // 设置线程队列等待连接个数
-                .childOption(ChannelOption.SO_KEEPALIVE, true) // 设置保持活动连接状态
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    // 给pipeline 设置处理器
-                    @Override
-                    protected void initChannel(SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(null);
-                    }
-                }); // 给我们的workGroup 的EventLoop 对应的管道设置处理器
 
-        System.out.println("... 服务器 is ready...");
-        // 绑定一个端口并且同步，生成一个 ChannelFuture 对象
-        ChannelFuture cf = bootstrap.bind(6668).sync();
+        try {
+            // 使用链式编程来进行设置
+            bootstrap.group(bossGroup, workerGroup) // 设置两个线程组
+                    .channel(NioServerSocketChannel.class) // 使用 NioServerSocketChannel 作为服务器的通道实现
+                    .option(ChannelOption.SO_BACKLOG, 128) // 设置线程队列等待连接个数
+                    .childOption(ChannelOption.SO_KEEPALIVE, true) // 设置保持活动连接状态
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        // 给pipeline 设置处理器
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    }); // 给我们的workGroup 的EventLoop 对应的管道设置处理器
 
-        // 对关闭通道进行监听
-        cf.channel().closeFuture().sync();
+            System.out.println("... 服务器 is ready...");
+            // 绑定一个端口并且同步，生成一个 ChannelFuture 对象
+            ChannelFuture cf = bootstrap.bind(6668).sync();
+
+            // 对关闭通道进行监听
+            cf.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
 
     }
 }
