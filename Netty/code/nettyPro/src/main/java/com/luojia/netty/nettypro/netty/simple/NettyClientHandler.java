b9173e98395eaa057e3593f315a7b3156de9265c
romantic <1371910434@qq.com>
2024-03-18 21:30:52 +0800

    netty 入门客户端Handler 处理器

---
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyClientHandler.java
@@ -0,0 +1,44 @@
+package com.luojia.netty.nettypro.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.StandardCharsets;
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当通道就绪就触发该方法
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("client " + ctx);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello, server: 你好", StandardCharsets.UTF_8));
+    }
+
+    /**
+     * 读取数据，当通道有数据时触发
+     * @param ctx 上下文对象，含有管道pipeline， 通道Channel，地址
+     * @param msg 客户端发送的数据，默认Object
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("client ctx = " + ctx);
+        // 将 msg 转成一个ByteBuf
+        // ByteBuf 是Netty 提供的，不是NIO的 ByteBuffer
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("服务器回复的消息：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("服务器地址：" + ctx.channel().remoteAddress());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
