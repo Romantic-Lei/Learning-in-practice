f034fc4694b2fdb0d284f16380377a1115113389
romantic <1371910434@qq.com>
2024-03-18 21:31:30 +0800

    netty 入门客户端

---
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyClient.java
@@ -0,0 +1,42 @@
+package com.luojia.netty.nettypro.netty.simple;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 客户端需要一个事件循环组
+        NioEventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            // 创建客户端启动对象
+            // 注意客户端使用的不是 ServerBootstrap 而是 Bootstrap
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group) // 设置线程组
+                    .channel(NioSocketChannel.class) // 设置客户端通道的视线类(反射)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            // 加入自己的处理器
+                            socketChannel.pipeline().addLast(new NettyClientHandler());
+                        }
+                    });
+
+            System.out.println("客户端 OK ...");
+
+            // 启动客户端连接服务端
+            // 关于 ChannelFuture 要分析，设计到netty 的异步模型
+            ChannelFuture channelFuture = bootstrap.connect("127.0.0.1", 6668).sync();
+            // 对关闭进行监听
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
