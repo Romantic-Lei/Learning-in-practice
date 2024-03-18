3c576ed6cb5de8163b29bd1b56f2f3269d5b36a6
romantic <1371910434@qq.com>
2024-03-18 20:21:20 +0800

    netty 入门服务端

---
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyServer.java
@@ -0,0 +1,44 @@
+package com.luojia.netty.nettypro.netty.simple;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class NettyServer {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 创建bossGroup和workGroup
+        // bossGroup只是处理连接请求，真正的和客户端业务处理请求会交给workerGroup完成
+        // 两个都是无限循环
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // 创建服务器端的启动对象，配置参数
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        // 使用链式编程来进行设置
+        bootstrap.group(bossGroup, workerGroup) // 设置两个线程组
+                .channel(NioServerSocketChannel.class) // 使用 NioServerSocketChannel 作为服务器的通道实现
+                .option(ChannelOption.SO_BACKLOG, 128) // 设置线程队列等待连接个数
+                .childOption(ChannelOption.SO_KEEPALIVE, true) // 设置保持活动连接状态
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    // 给pipeline 设置处理器
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(null);
+                    }
+                }); // 给我们的workGroup 的EventLoop 对应的管道设置处理器
+
+        System.out.println("... 服务器 is ready...");
+        // 绑定一个端口并且同步，生成一个 ChannelFuture 对象
+        ChannelFuture cf = bootstrap.bind(6668).sync();
+
+        // 对关闭通道进行监听
+        cf.channel().closeFuture().sync();
+
+    }
+}
