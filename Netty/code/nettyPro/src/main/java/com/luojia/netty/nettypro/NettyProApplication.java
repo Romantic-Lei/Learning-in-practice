f8d429d463f5e1d49c08bf76269fe82a216ac214
romantic <1371910434@qq.com>
2024-03-18 22:10:49 +0800

    工作原理详解

---
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/NettyProApplication.java
@@ -0,0 +1,13 @@
+package com.luojia.netty.nettypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyProApplication.class, args);
+    }
+
+}
