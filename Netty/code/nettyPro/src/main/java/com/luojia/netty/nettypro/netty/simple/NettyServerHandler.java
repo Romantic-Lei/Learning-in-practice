a0191aee856bacc37b7b1b234eecdcb0146c1fc7
romantic <1371910434@qq.com>
2024-03-18 20:45:23 +0800

    netty 入门Handler 处理器

---
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/simple/NettyServerHandler.java
@@ -0,0 +1,48 @@
+package com.luojia.netty.nettypro.netty.simple;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 我们自定义一个Handler 需要继承netty 规定好的某个HandlerAdapter(规范)
+ * 这时我们自定义一个Handler，才能称为一个handler
+ */
+public class NettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 读取数据（这里读取客户端发送的消息）
+     * @param ctx 上下文对象，含有管道pipeline， 通道Channel，地址
+     * @param msg 客户端发送的数据，默认Object
+     * @throws Exception
+     */
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("server ctx = " + ctx);
+        // 将 msg 转成一个ByteBuf
+        // ByteBuf 是Netty 提供的，不是NIO的 ByteBuffer
+        ByteBuf buf = (ByteBuf) msg;
+        System.out.println("客户端发送消息是：" + buf.toString(CharsetUtil.UTF_8));
+        System.out.println("客户端地址：" + ctx.channel().remoteAddress());
+    }
+
+    /**
+     * 数据读取完后，执行的操作
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // writeAndFlush 是 write和flush
+        // 将数据写入缓存并刷新，一般我们需要对发送的数据进行编码
+        ctx.writeAndFlush(Unpooled.copiedBuffer("hello, 客户端", CharsetUtil.UTF_8));
+    }
+
+    // 处理异常，需要关闭通道
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
