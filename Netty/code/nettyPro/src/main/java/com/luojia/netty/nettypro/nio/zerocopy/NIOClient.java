ccd36ebcdd3fa8942f26f6dc16fb7aea8ba6d091
romantic <1371910434@qq.com>
2024-03-21 23:29:25 +0800

    NIO初识

--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/zerocopy/NIOClient.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/zerocopy/NIOClient.java
@@ -1,2 +1,26 @@
-package com.luojia.netty.nettypro.nio.zerocopy;public class NIOClient {
+package com.luojia.netty.nettypro.nio.zerocopy;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+public class NIOClient {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress("127.0.0.1", 7001));
+        String fileName = "F:\\nginx-1.23.4.zip";
+        FileChannel channel = new FileInputStream(fileName).getChannel();
+        
+        // 准备发送
+        long start = System.currentTimeMillis();
+        // 在Linux下一个transferTo 方法就可以完成传输
+        // 在Windows下一个transferTo 只能发送8M，需要分段传输文件，需要注意传输的位置
+        // transferTo 底层使用到零拷贝
+        long transferCount = channel.transferTo(0, channel.size(), socketChannel);
+
+        System.out.println("发送的总的字节数 = " + transferCount + "  耗时：" + (System.currentTimeMillis() - start));
+        channel.close();
+    }
 }
--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/zerocopy/NIOServer.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/zerocopy/NIOServer.java
@@ -1,2 +1,27 @@
-package com.luojia.netty.nettypro.nio.zerocopy;public class NIOServer {
+package com.luojia.netty.nettypro.nio.zerocopy;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+public class NIOServer {
+    public static void main(String[] args) throws IOException {
+        InetSocketAddress address = new InetSocketAddress(7001);
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.socket().bind(address);
+
+        // 创建Buffer
+        ByteBuffer buffer = ByteBuffer.allocate(4096);
+        while (true) {
+            SocketChannel socketChannel = serverSocketChannel.accept();
+            int readCount = 0;
+            while (-1 != readCount) {
+                readCount = socketChannel.read(buffer);
+            }
+            buffer.rewind(); // 倒带，让数据可以重读
+        }
+
+    }
 }
