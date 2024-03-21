b6887fd631225cf1dfaec1cbeb8421efa9d9de61
romantic <1371910434@qq.com>
2024-03-21 23:29:04 +0800

    NIO文件操作

--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/NIOFileChannel01.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/NIOFileChannel01.java
@@ -1,2 +1,26 @@
-package com.luojia.netty.nettypro.nio;public class NIOFileChannel01 {
+package com.luojia.netty.nettypro.nio;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NIOFileChannel01 {
+    public static void main(String[] args) throws IOException {
+        String str = "你好，世界";
+        FileOutputStream fileOutputStream = new FileOutputStream("D:\\file01.txt");
+
+        // 通过fileOutputStream 获取对应的FileChannel
+        FileChannel fileChannel = fileOutputStream.getChannel();
+
+        // 创建一个缓冲区 ByteBuffer
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        // 将数据写入到缓冲区
+        byteBuffer.put(str.getBytes());
+        // 对ByteBuffer 进行flip
+        byteBuffer.flip();
+
+        fileChannel.write(byteBuffer);
+        fileOutputStream.close();
+    }
 }
--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/NIOFileChannel02.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/nio/NIOFileChannel02.java
@@ -1,2 +1,24 @@
-package com.luojia.netty.nettypro.nio;public class NIOFileChannel02 {
+package com.luojia.netty.nettypro.nio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NIOFileChannel02 {
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("D:\\file01.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        FileChannel fileChannel = fileInputStream.getChannel();
+
+        // 创建一个缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate((int)file.length());
+        // 将通道数据读入到缓冲区
+        fileChannel.read(byteBuffer);
+        System.out.println(new String(byteBuffer.array()));
+        fileChannel.close();
+    }
 }
