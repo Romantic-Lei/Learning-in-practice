b91501a8d8446b1e0f6de928454795125fd635fd
romantic <1371910434@qq.com>
2024-04-13 23:01:19 +0800

    netty 连接超时

--- Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/basicapi/TestConnectionTimeout.java
+++ Netty/code/nettyPro/src/main/java/com/luojia/netty/nettypro/netty/basicapi/TestConnectionTimeout.java
@@ -1,2 +1,37 @@
-package com.luojia.netty.nettypro.netty.basicapi;public class TestConnectionTimeout {
+package com.luojia.netty.nettypro.netty.basicapi;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TestConnectionTimeout {
+    public static void main(String[] args) {
+        // ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // 是给 ServerSocketChannel 配置参数
+        // ServerBootstrap option = serverBootstrap.option();
+        // 是给 SocketChannel 配置参数
+        // serverBootstrap.childOption();
+
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 300)
+                    .channel(NioSocketChannel.class)
+                    .handler(new LoggingHandler());
+            ChannelFuture future = bootstrap.connect("localhost", 6668);
+            future.sync().channel().closeFuture().sync(); // 断点1
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.debug("timeout");
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
 }
