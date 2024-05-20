ee1ce7d4ff59e0d3c72264e31d95cc9027ade8db
romantic <1371910434@qq.com>
2024-05-20 22:02:37 +0800

    定时查询超时订单

---
+++ 在线支付/payment/src/main/java/com/luojiapay/payment/task/WxPayTask.java
@@ -0,0 +1,34 @@
+package com.luojiapay.payment.task;
+
+import com.luojiapay.payment.entity.OrderInfo;
+import com.luojiapay.payment.service.OrderInfoService;
+import com.luojiapay.payment.service.WxPayService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+public class WxPayTask {
+
+    @Autowired
+    OrderInfoService orderInfoService;
+    @Autowired
+    WxPayService wxPayService;
+
+    @Scheduled(cron = "0/30 * * * * ?")
+    public void orderConfirm() {
+        List<OrderInfo> orderInfoList = orderInfoService.getNoPayOrderByDuration(5);
+        for (OrderInfo orderInfo : orderInfoList) {
+            String orderNo = orderInfo.getOrderNo();
+            log.warn("超过订单====>{}", orderNo);
+
+            // 核实订单状态，调用微信支付查单接口
+            wxPayService.checkOrderStatus(orderNo);
+        }
+    }
+
+}
