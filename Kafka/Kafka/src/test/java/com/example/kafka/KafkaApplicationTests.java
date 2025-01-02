97959ca29e3e0e441d8cf748c3ca557ecf3b3af9
romantic <1371910434@qq.com>
2025-01-02 15:43:19 +0800

    配置文件

---
+++ Kafka/Kafka/src/test/java/com/example/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
