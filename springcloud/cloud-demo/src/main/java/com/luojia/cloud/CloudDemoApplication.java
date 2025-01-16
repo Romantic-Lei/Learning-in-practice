bab82911e0021854aa73bc11e4c7e80903b865b8
romantic <1371910434@qq.com>
2025-01-16 21:09:08 +0800

    spring-cloud 服务搭建

---
+++ springcloud/cloud-demo/src/main/java/com/luojia/cloud/CloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.luojia.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoApplication.class, args);
+    }
+
+}
