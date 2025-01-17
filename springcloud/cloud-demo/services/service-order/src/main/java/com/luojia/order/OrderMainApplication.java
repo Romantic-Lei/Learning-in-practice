677fa1dd59bc66bdaaeaa61eeea2f570dc0e88a5
romantic <1371910434@qq.com>
2025-01-17 20:28:31 +0800

    spring-cloud 服务搭建

---
+++ springcloud/cloud-demo/services/service-order/src/main/java/com/luojia/order/OrderMainApplication.java
@@ -0,0 +1,13 @@
+package com.luojia.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+
+}
---
+++ springcloud/cloud-demo/services/service-product/src/main/java/com/luojia/product/ProductMainApplication.java
@@ -0,0 +1,13 @@
+package com.luojia.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class, args);
+    }
+
+}
