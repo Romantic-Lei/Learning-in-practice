84ae72ab7a019f1294f07895cf9eacf9ac49687a
romantic <1371910434@qq.com>
2025-02-15 14:25:02 +0800

    网关服务

---
+++ springcloud/cloud-demo/gateway/src/main/java/com/luojia/gateway/GatewayMainApplication.java
@@ -0,0 +1,15 @@
+package com.luojia.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMainApplication.class, args);
+    }
+
+}
